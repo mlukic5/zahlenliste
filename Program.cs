afe4736c698ad9749a0f23db0972ce64e3446abc
Manuel Lukic <mlukic@schueler.hakzell.at>
2024-01-12 11:39:21 +0100

    add .gitignore

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using zahlenliste;
+
+List<int> Zahl = new List<int>(5);
+
+Zahl.Add(1);
+Zahl.Add(2);
+Zahl.Add(3);
+Zahl.Add(4);
+Zahl.Add(5);
+
+int summe = Zahl[0] + Zahl[1] + Zahl[2] + Zahl[3] + Zahl[4];
+
+Console.WriteLine($"Die Summe beträgt: {summe}");
